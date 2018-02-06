d055e9670b554f0c8cfc959a6f72f22eddd970f2
ArnavPalkhiwala <league@iMac.attlocal.net>
2018-02-05 18:28:19 -0800

    NBA Scoreboard and Control Panel (Both IP)

--- src/NBAControlPanel.java
+++ src/NBAControlPanel.java
@@ -1,12 +1,15 @@
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class NBAControlPanel {
+public class NBAControlPanel implements ActionListener {
 	
 	JFrame controlPanel = new JFrame();
 	JPanel  MAIN = new JPanel();
@@ -21,21 +24,10 @@ public class NBAControlPanel {
 	JPanel homeTOS = new JPanel();
 	JPanel awayTOS = new JPanel();
 	
-		
-
-
-
-
-			
-		
-
-	
-	
+	NBAScoreboard scoreboard = new NBAScoreboard();
 	
 	public static void main(String[] args) {
 		
-		NBAScoreboard scoreboard = new NBAScoreboard();
-		scoreboard.run();
 		NBAControlPanel cp = new NBAControlPanel();
 		cp.panelRun();
 
@@ -45,69 +37,96 @@ public class NBAControlPanel {
 
 	void panelRun(){
 		
+		scoreboard.run();
 		JFrame Control = new JFrame();
 		JPanel Main = new JPanel();
+		Main.setLayout(new BorderLayout());
 		Control.add(Main);
 		Control.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				
 		JPanel Home = new JPanel();
 		JPanel Away = new JPanel();
-		Home.setLayout(new BorderLayout());
-		Away.setLayout(new BorderLayout());
+		Home.setLayout(new BoxLayout(Home, BoxLayout.PAGE_AXIS));
+
+		Away.setLayout(new BoxLayout(Away, BoxLayout.PAGE_AXIS));
+		
+		Main.add(Home, BorderLayout.WEST);
+		Main.add(Away, BorderLayout.EAST);
+
 		JPanel HomeFouls = new JPanel();
 		HomeFouls.setLayout(new BorderLayout());
 		JPanel AwayFouls = new JPanel();
 		AwayFouls.setLayout(new BorderLayout());
-		Home.setLayout(new BorderLayout());
-		Home.add(HomeFouls, BorderLayout.NORTH);
-		Away.add(AwayFouls, BorderLayout.NORTH);
+		
+		
 		JPanel HomeTimeouts = new JPanel();
 		HomeTimeouts.setLayout(new BorderLayout());
 		JPanel AwayTimeouts = new JPanel();
 		AwayTimeouts.setLayout(new BorderLayout());
-		Main.add(Home, BorderLayout.WEST);
-		Main.add(Away, BorderLayout.EAST);
-		Main.add(HomeTimeouts, BorderLayout.CENTER);
-		Main.add(AwayTimeouts, BorderLayout.CENTER);
+		
 		JTextField home = new JTextField();	
 		JTextField away = new JTextField();	
 		home.setText("Home");
 		away.setText("Visitors");
-		Home.add(home, BorderLayout.NORTH);
-		Away.add(away, BorderLayout.NORTH);
+		Home.add(home);
+		Away.add(away);
+		
 		JLabel hpoints = new JLabel("Points:");
 		JLabel vpoints = new JLabel("Points:");
-		Home.add(hpoints, BorderLayout.CENTER);
-		Away.add(vpoints, BorderLayout.CENTER);
-		JButton addpoints = new JButton("+1");
-		Home.add(addpoints, BorderLayout.SOUTH);
-		JButton minuspoints = new JButton("-1");
-		Home.add(minuspoints, BorderLayout.SOUTH);
-		Away.add(addpoints);
-		Away.add(minuspoints);
+		Home.add(hpoints);
+		Away.add(vpoints);
+		JButton haddpoints = new JButton("+1");
+		haddpoints.addActionListener(this);
+		JButton hminuspoints = new JButton("-1");
+		hminuspoints.addActionListener(this);
+		JButton vaddpoints = new JButton("+1");
+		vaddpoints.addActionListener(this);
+		JButton vminuspoints = new JButton("-1");
+		vminuspoints.addActionListener(this);
+		Home.add(haddpoints);
+		Home.add(hminuspoints);
+		Away.add(vaddpoints);
+		Away.add(vminuspoints);
+		
+		Home.add(HomeFouls);
+		Away.add(AwayFouls);
 		JLabel hfouls = new JLabel("Fouls:");
 		JLabel vfouls = new JLabel("Fouls:");
 		HomeFouls.add(hfouls, BorderLayout.NORTH);
 		AwayFouls.add(vfouls, BorderLayout.NORTH);
-		JButton addFouls = new JButton("+1");
-		JButton minusFouls = new JButton("-1");
-		HomeFouls.add(addFouls);
-		HomeFouls.add(minusFouls);
-		AwayFouls.add(addFouls);
-		AwayFouls.add(minusFouls);
+		JButton haddFouls = new JButton("+1");
+		JButton hminusFouls = new JButton("-1");
+		JButton vaddFouls = new JButton("+1");
+		JButton vminusFouls = new JButton("-1");
+		HomeFouls.add(haddFouls, BorderLayout.CENTER);
+		HomeFouls.add(hminusFouls, BorderLayout.SOUTH);
+		AwayFouls.add(vaddFouls, BorderLayout.CENTER);
+		AwayFouls.add(vminusFouls, BorderLayout.SOUTH);
+		
+		Home.add(HomeTimeouts);
+		Away.add(AwayTimeouts);
 		JLabel htimeouts = new JLabel("Timeouts:");
 		JLabel vtimeouts = new JLabel("Timeouts:");
-		HomeTimeouts.add(htimeouts);
-		AwayTimeouts.add(vtimeouts);
-		JButton addTimeouts = new JButton("+1");
-		JButton minusTimeouts = new JButton("-1");
-		HomeTimeouts.add(addTimeouts);
-		HomeTimeouts.add(minusTimeouts);
+		HomeTimeouts.add(htimeouts, BorderLayout.NORTH);
+		AwayTimeouts.add(vtimeouts, BorderLayout.NORTH);
+		JButton haddTimeouts = new JButton("+1");
+		JButton hminusTimeouts = new JButton("-1");
+		JButton vaddTimeouts = new JButton("+1");
+		JButton vminusTimeouts = new JButton("-1");
+		HomeTimeouts.add(haddTimeouts, BorderLayout.CENTER);
+		HomeTimeouts.add(hminusTimeouts, BorderLayout.SOUTH);
+		AwayTimeouts.add(vaddTimeouts, BorderLayout.CENTER);
+		AwayTimeouts.add(vminusTimeouts, BorderLayout.SOUTH);
+		
 		Control.setVisible(true);
-		Control.pack();
-		
-		
-		
-		
+		Control.pack(); 
+		}
+
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
 		
 	}
 }
